186cf5153c674ac387115e5f37ae8e7785db5dc1
aryehkd <ari.delugach42@gmail.com>
2024-02-02 16:20:56 +0000

    testing/linting/action setup

--- apps/mystacks-web/src/app/components/account-login-form/account-login-form.spec.tsx
+++ apps/mystacks-web/src/app/components/account-login-form/account-login-form.spec.tsx
@@ -1,7 +1,11 @@
 import { render } from '@testing-library/react';
 
 import { AccountLoginFormStory } from './account-login-form.stories';
+import { NavigateOptions, To } from 'react-router-dom';
 
+vi.mock('react-router-dom', () => ({
+  useNavigate: (to: To, options?: NavigateOptions): void => {}
+}));
 describe('AccountLoginForm', () => {
   it('should render successfully', () => {
 
